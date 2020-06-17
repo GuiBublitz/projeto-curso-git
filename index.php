c7b2f65aa8572144f69f23b0582f4d6d9e9fd438
Guilherme Eduardo Bubliz <60118323+GuiBublitz@users.noreply.github.com>
2020-06-17 16:09:20 -0300

    otimização de site e adiçao de pagina 404

--- index.php
+++ index.php
@@ -37,87 +37,20 @@
 		</div><!--center-->
 	</header>
 
-	<section class="banner-principal">
-		<div class="overlay"></div><!--overlay-->
-		<div class="center">
-			<form>
-				<h2>Qual o seu  melhor e-mail?</h2>
-				<input type="email" name="email" required/>
-				<input type="submit" name="acao" value="Cadastrar!"/>
-			</form>
-		</div><!--center-->
-	</section><!--banner-principal-->
+	<?php
 
-	<section class="descricao-autor">
-		<div class="center">
-			<div  class="w50 left">
-				<h2>Guilherme Eduardo Bublitz</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel metus rhoncus, molestie ante ut, ornare sapien. Donec posuere mauris leo. Praesent condimentum ante id dignissim venenatis. Etiam blandit enim eu tellus dignissim vulputate at non risus. Sed luctus sem ut ex consequat, ut condimentum augue venenatis. Duis viverra sagittis leo, a sollicitudin lectus aliquam vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent pellentesque eget mi ut ullamcorper.</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel metus rhoncus, molestie ante ut, ornare sapien. Donec posuere mauris leo. Praesent condimentum ante id dignissim venenatis. Etiam blandit enim eu tellus dignissim vulputate at non risus. Sed luctus sem ut ex consequat, ut condimentum augue venenatis. Duis viverra sagittis leo, a sollicitudin lectus aliquam vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent pellentesque eget mi ut ullamcorper.</p>
-		 	</div><!--w50-->
-		 	<div  class="w50 left">
-				<!--pegar imagem depois-->
-				<img class="right" src="<?php echo INCLUDE_PATH; ?>images/Gui.jpg"/>
-		 	</div><!--w50-->
-		 	<div class="clear"></div><!--clear-->
-	    </div><!--center-->
-	</section><!--derscrição autor-->
+		$url = isset($_GET['url']) ? $_GET['url'] : 'home';
 
-	<section class="especialidades">
-		<div class="center">
-			<h2 class="title">Especialidades</h2>
-			<div class="w33 left box-especialidades">
-				<h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
-				<h4>CSS3</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit vitae tortor suscipit viverra. Vestibulum diam nisl, pellentesque quis nisi ac, dapibus tincidunt turpis. Donec rhoncus massa eros, ut feugiat ante scelerisque et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque sit amet ornare nisl. Morbi eu orci a ex malesuada vestibulum in nec leo. Integer eget dolor elit. Nunc eget massa in lacus convallis dictum. Maecenas lectus augue, imperdiet in ante eu, mollis mattis turpis. Etiam at lectus sed quam dapibus malesuada a molestie tortor. Sed accumsan sodales lectus, vitae viverra nibh iaculis ut. Nam gravida risus nec tempor molestie. Nunc viverra dignissim sollicitudin. Praesent et commodo nibh, nec malesuada mi.</p>	
-			</div><!--box-especialidade-->
-			<div class="w33 left box-especialidades">
-				<h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
-				<h4>HTML5</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit vitae tortor suscipit viverra. Vestibulum diam nisl, pellentesque quis nisi ac, dapibus tincidunt turpis. Donec rhoncus massa eros, ut feugiat ante scelerisque et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque sit amet ornare nisl. Morbi eu orci a ex malesuada vestibulum in nec leo. Integer eget dolor elit. Nunc eget massa in lacus convallis dictum. Maecenas lectus augue, imperdiet in ante eu, mollis mattis turpis. Etiam at lectus sed quam dapibus malesuada a molestie tortor. Sed accumsan sodales lectus, vitae viverra nibh iaculis ut. Nam gravida risus nec tempor molestie. Nunc viverra dignissim sollicitudin. Praesent et commodo nibh, nec malesuada mi.</p>	
-			</div><!--box-especialidade-->
-			<div class="w33 left box-especialidades">
-				<h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
-				<h4>JavaScript</h4>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis blandit vitae tortor suscipit viverra. Vestibulum diam nisl, pellentesque quis nisi ac, dapibus tincidunt turpis. Donec rhoncus massa eros, ut feugiat ante scelerisque et. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque sit amet ornare nisl. Morbi eu orci a ex malesuada vestibulum in nec leo. Integer eget dolor elit. Nunc eget massa in lacus convallis dictum. Maecenas lectus augue, imperdiet in ante eu, mollis mattis turpis. Etiam at lectus sed quam dapibus malesuada a molestie tortor. Sed accumsan sodales lectus, vitae viverra nibh iaculis ut. Nam gravida risus nec tempor molestie. Nunc viverra dignissim sollicitudin. Praesent et commodo nibh, nec malesuada mi.</p>	
-			</div><!--box-especialidades-->
-			<div class="clear"></div><!--clear-->
-		</div><!--center-->	
-	</section><!--especialidades-->
+		if (file_exists('pages/'.$url.'.php')){
+			include('pages/'.$url.'.php');
+		}else{
+			//Podemos fazer oq quiser porque a página não existe.
+			$pagina404 = true;
+			include('pages/404.php');
+		}
+	?>
 
-	<section class="extras">
-
-		<div class="center">
-			<div class="w50 left depoimentos-container">
-			 	<h2 class="title">Depoimentos dos nosso clientes</h2>
-			 	<div class="depoimento-single">
-			 		<p class="depoimento-descricao">"Fusce porta aliquet lacus id congue. Integer rhoncus arcu et eleifend pretium. Sed id laoreet mauris. Suspendisse at justo non arcu auctor venenatis vitae ac tellus. Nulla faucibus est ac nibh cursus, at viverra justo porta. In placerat molestie scelerisque. Etiam sed lorem eu nisi molestie faucibus ac vel sem. Ut molestie dictum ex quis accumsan. Sed quis mollis est, a luctus magna. Integer ut porttitor sem, sit amet tempus lorem. Duis non lorem eleifend, tristique leo a, luctus sapien. Phasellus in tincidunt nisl, sit amet commodo elit. Quisque in porttitor orci. Nunc sagittis sem at aliquam sagittis."</p>
-			 		<p class="nome-autor">Lorem Ipsum</p>
-			 	</div><!--depoimento-single-->
-			 	<div class="depoimento-single">
-			 		<p class="depoimento-descricao">"Fusce porta aliquet lacus id congue. Integer rhoncus arcu et eleifend pretium. Sed id laoreet mauris. Suspendisse at justo non arcu auctor venenatis vitae ac tellus. Nulla faucibus est ac nibh cursus, at viverra justo porta. In placerat molestie scelerisque. Etiam sed lorem eu nisi molestie faucibus ac vel sem. Ut molestie dictum ex quis accumsan. Sed quis mollis est, a luctus magna. Integer ut porttitor sem, sit amet tempus lorem. Duis non lorem eleifend, tristique leo a, luctus sapien. Phasellus in tincidunt nisl, sit amet commodo elit. Quisque in porttitor orci. Nunc sagittis sem at aliquam sagittis."</p>
-			 		<p class="nome-autor">Lorem Ipsum</p>
-			 	</div><!--depoimento-single-->
-			 	<div class="depoimento-single">
-			 		<p class="depoimento-descricao">"Fusce porta aliquet lacus id congue. Integer rhoncus arcu et eleifend pretium. Sed id laoreet mauris. Suspendisse at justo non arcu auctor venenatis vitae ac tellus. Nulla faucibus est ac nibh cursus, at viverra justo porta. In placerat molestie scelerisque. Etiam sed lorem eu nisi molestie faucibus ac vel sem. Ut molestie dictum ex quis accumsan. Sed quis mollis est, a luctus magna. Integer ut porttitor sem, sit amet tempus lorem. Duis non lorem eleifend, tristique leo a, luctus sapien. Phasellus in tincidunt nisl, sit amet commodo elit. Quisque in porttitor orci. Nunc sagittis sem at aliquam sagittis."</p>
-			 		<p class="nome-autor">Lorem Ipsum</p>
-			 	</div><!--depoimento-single-->
-		    </div><!--w50-->
-		    <div class="w50 left servicos-container">
-			 	<h2 class="title">Serviços</h2>
-			 	<div class="servicos">
-			 		<ul>
-			 			<li>Proin pulvinar, odio at viverra bibendum, mi arcu mollis libero, in congue lectus dui sit amet ipsum. Maecenas commodo metus dui, sagittis mattis lacus condimentum eget. Pellentesque malesuada massa vel eros tincidunt faucibus. Cras venenatis pharetra molestie. Duis pellentesque in est eget vulputate. Nullam finibus maximus tellus, eu sollicitudin erat aliquam eget. Etiam sed purus non sapien finibus pharetra eget quis nisl. Pellentesque fermentum, orci at lacinia interdum, quam nisi tempor urna, non laoreet mi elit sed metus. Suspendisse id ultrices ligula, porttitor sodales nulla.</li>
-			 			<li>Proin pulvinar, odio at viverra bibendum, mi arcu mollis libero, in congue lectus dui sit amet ipsum. Maecenas commodo metus dui, sagittis mattis lacus condimentum eget. Pellentesque malesuada massa vel eros tincidunt faucibus. Cras venenatis pharetra molestie. Duis pellentesque in est eget vulputate. Nullam finibus maximus tellus, eu sollicitudin erat aliquam eget. Etiam sed purus non sapien finibus pharetra eget quis nisl. Pellentesque fermentum, orci at lacinia interdum, quam nisi tempor urna, non laoreet mi elit sed metus. Suspendisse id ultrices ligula, porttitor sodales nulla.</li>
-			 			<li>Proin pulvinar, odio at viverra bibendum, mi arcu mollis libero, in congue lectus dui sit amet ipsum. Maecenas commodo metus dui, sagittis mattis lacus condimentum eget. Pellentesque malesuada massa vel eros tincidunt faucibus. Cras venenatis pharetra molestie. Duis pellentesque in est eget vulputate. Nullam finibus maximus tellus, eu sollicitudin erat aliquam eget. Etiam sed purus non sapien finibus pharetra eget quis nisl. Pellentesque fermentum, orci at lacinia interdum, quam nisi tempor urna, non laoreet mi elit sed metus. Suspendisse id ultrices ligula, porttitor sodales nulla.</li>
-					</ul>
-			 	</div><!--servicos-->	
-		    </div><!--w50-->
-		    <div class="clear"></div>
-		</div><!--center-->
-	</section><!--extras-->
-
-	<footer>
+	<footer <?php if(isset($pagina404) && $pagina404 == true ) echo 'class="fixed" ' ?>>
 		<div class="center">
 			<p>Todos os direitos reservados</p>
 		</div><!--center-->
---
+++ pages/404.php
@@ -0,0 +1,9 @@
+<section class="erro_404">
+	<div class="center">
+
+	<div class="wraper_404">
+		<h2><i class="fa fa-exclamation-circle"></i> A Página NÃO Existe <i class="fa fa-exclamation-circle"></i></h2> 
+		<p>Deseja voltar para a <a href="<?php echo INCLUDE_PATH; ?>" >página inicial?</a></p>
+	</div><!--wraper_404-->
+	</div><!--center-->
+</section>
